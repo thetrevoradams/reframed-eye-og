6df0a7153d6263096c1e430d0eaa8106270012b1
Trevor Adams <contact@thetrevoradams.com>
2024-09-16 22:58:16 -0600

    ensure meta tags are applied separate from og ones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import './globals.css';
 import { manrope, marcellus } from './fonts';
 
 export const metadata: Metadata = {
+  title: 'Reframed Eye Optometry',
+  description:
+    'Comprehensive eye care services at Reframed Eye, a new optometry practice coming soon to Idaho Falls, Idaho. Glasses, contacts, eye exams, low vision evaluations and more! Schedule online or call 208-557-3222.',
   openGraph: {
     type: 'website',
     url: 'https://reframedeye.com',
